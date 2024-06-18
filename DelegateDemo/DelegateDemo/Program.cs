24764498533aeffbd5ca11186f0b36f0530a1a11
Priya Kore <163241863+himu-2023@users.noreply.github.com>
2024-06-18 18:36:13 +0530

    Add files via upload

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using DelegatesLibrary;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Products p = new Products();
+        ProductIDGeneration delObj = new ProductIDGeneration(p.GenerateId);
+        int productid =delObj.Invoke();
+
+        Console.WriteLine(productid);
+        Console.WriteLine("=================================");
+        productid = delObj.Invoke();
+        Console.WriteLine(productid);
+        Console.WriteLine("=================================");
+        WorkingWithProductData[] delObj1 = new WorkingWithProductData[2] { p.AddProduct, p.UpdateProduct };
+        WorkingWithProductData del=(WorkingWithProductData)MulticastDelegate.Combine(delObj1);
+        del();
+
+
+    }
+}
---
+++ DelegateDemo/DelegatesLibrary/Products.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegatesLibrary
+{
+    public  delegate int ProductIDGeneration();
+    public delegate void WorkingWithProductData();
+    public class Products
+    {
+        static int cnt = 0;
+        public int GenerateId()
+        {
+            return ++cnt;
+        }
+        public void AddProduct()
+        {
+            Console.WriteLine("Product addd called");
+        }
+        public void UpdateProduct()
+        {
+            Console.WriteLine("Update Product called");
+        }
+    }
+}
