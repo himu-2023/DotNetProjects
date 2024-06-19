275c5d3ac724b1328e258a237d370ac1cf6f129a
Priya Kore <163241863+himu-2023@users.noreply.github.com>
2024-06-19 18:43:45 +0530

    Add files via upload

---
+++ ExceptionHandlingDemo/ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExceptionProjectLibrary;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+		Customer c = new Customer();
+		try
+		{
+			Console.WriteLine("Enter the customer id");
+			c.Custid = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter the customer name");
+			c.CustName = Console.ReadLine();
+            Console.WriteLine("Enter the customer city");
+            c.City = Console.ReadLine();
+        }
+		catch (ArgumentNullException ex)
+		{
+			Console.WriteLine(ex.Message);
+			
+		}
+        catch (AccessViolationException ex)
+        {
+            Console.WriteLine(ex.Message);
+
+
+        }
+        catch (CustomerNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+
+        }
+        finally
+		{
+			Console.WriteLine("Finally block...");
+		}
+    }
+}
---
+++ ExceptionHandlingDemo/ExceptionProjectLibrary/Customer.cs
@@ -0,0 +1,83 @@
+﻿//using System.Runtime.Serialization.Json;
+//using System.Xml.Serialization;
+//using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ExceptionProjectLibrary
+{
+    public class Customer
+    {
+        int _custid;
+        string _custname;
+        string _city;
+        public int Custid
+        {
+            get 
+            { 
+                
+                return _custid;
+            }
+            set 
+            {
+                if (value > 0 && (value < 2000))
+                {
+                    _custid = value;
+                   
+                    
+                }
+                else if (value>2000)
+                {
+                    throw new CustomerNotFoundException("This is invalid");
+                }
+                else
+                {
+                    throw new ArgumentNullException("Customer id must be a +ve number");                                                                            
+                }
+               
+            } 
+        }
+        public string CustName
+        { 
+            get
+            { 
+                return _custname;
+            } 
+                set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Customer name can not be null or empty");
+                }
+                else if (value.StartsWith(" "))
+                {
+                    throw new AccessViolationException("Name cannot starts with space");
+                }
+                else
+                {
+                    _custname = value;
+                }
+              
+            }
+        }
+        public string City 
+        {
+            get
+            {
+                return _city;
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("City can not be empty");
+                }
+                else
+                {
+                    _city = value;
+                }
+               
+            }
+        }
+
+    }
+}
